8532183556429f8e936a731fab7b4cd278ba5c13
Tom Bailey <tombailey@users.noreply.github.com>
2023-06-10 16:27:11 +0900

    Simplify health route

--- src/router/health.rs
+++ src/router/health.rs
@@ -1,6 +1,7 @@
 use actix_web::{get, HttpResponse, Responder};
 use actix_web::http::header::ContentType;
 use serde::Serialize;
+use serde_json::json;
 
 #[derive(Serialize)]
 struct Healthy {
@@ -9,12 +10,7 @@ struct Healthy {
 
 #[get("/health")]
 pub async fn get_health() -> impl Responder {
-    let healthy = Healthy {
-        status: String::from("pass")
-    };
-    let json = serde_json::to_string(&healthy).unwrap();
-
     HttpResponse::Ok()
         .content_type(ContentType::json())
-        .body(json)
+        .json(json!({ "status": "pass" }))
 }
